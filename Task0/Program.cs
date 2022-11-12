d23732ca266e4ab606f8c828fcc3770eacb53fd4
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-12 14:33:57 +0300

    Initial commit

---
+++ Task0/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine()); //преобразуем строку в число
+
+Console.WriteLine(number*number);
---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = (numberB*numberB);
+
+if (numberA == numberC)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
